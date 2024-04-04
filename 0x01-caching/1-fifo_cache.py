ed1b67a9415e9eb18594f593c0e2c294a3fd2b0f
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-04 02:01:36 +0000

    Completed task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,7 +17,7 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Assigns key value to cache_data"""
         if key is None or item is None:
-            pass
+            return
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
