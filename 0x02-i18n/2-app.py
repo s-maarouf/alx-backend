868746ef4df8fbeaa9615d45ab57deeccf531e31
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-09 23:06:38 +0000

    Completed task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Flask module"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from requests import request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """default babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Returns the locale that should be used for this request"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def get_index() -> str:
+    """renders index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
